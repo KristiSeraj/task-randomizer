f0c5c01d5b548f07aa44e5d8d262a8d7cf86e43e
KristiSeraj <serajkristi@gmail.com>
2022-08-24 21:54:12 +0200

    fix

--- src/js/random.js
+++
@@ -1,9 +0,0 @@
-function randomTask() {
-let bruh;
-let klasa = document.getElementsByClassName('listElement');
-console.log(klasa)
-for(var i=0;i < klasa.length; i++){
-        bruh = klasa[i].textContent.slice(0, -1);
-        console.log(bruh);
-    }
-}
--- src/js/script.js
+++ src/js/script.js
@@ -1,56 +1,8 @@
-let addButton = document.getElementById("addTask");
-let input = document.getElementById("task");
-let ul = document.querySelector("ul");
-let item = document.getElementsByTagName("li");
+let myTasks = document.getElementById("task");
+let myList = document.getElementById("listTask");
 
-function inputLength(){
-	return input.value.length;
-} 
-
-function listLength(){
-	return item.length;
+function addNewTask() {
+	let myListChild = document.createElement("li");
+	myListChild.appendChild(myTasks);
+	myList.appendChild(myListChild);
 }
-
-function createListElement() {
-	let li = document.createElement("li"); // creates an element "li"
-	li.setAttribute("class", "listElement")
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
-
-
-
-	// START ADD DELETE BUTTON
-	let dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("x"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteListItem);
-	// END ADD DELETE BUTTON
-
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
-	function deleteListItem(){
-		li.classList.add("delete")
-	}
-	//END ADD CLASS DELETE
-}
-
-
-function addListAfterClick(){
-	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
-		createListElement();
-	} 
-}
-
-
-addButton.addEventListener("click",addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
